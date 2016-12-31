8965ebdea98cd83c0275945cc5fc8fe2abbe35de
jmerkl <jacksoncmerkl@gmail.com>
2016-12-31 03:12:44 -0600

    Working Game Rev 1
    Got jumping & double jumping in.  Adding in side scrolling and then
    obstacles

---
+++ Spike.java
@@ -0,0 +1,123 @@
+import javafx.scene.image.Image;
+
+public class Spike {
+	
+	Image mcImage;
+	boolean jumping;
+	boolean doubleJumping;
+	double dxdt = 0.0;
+	double dydt = 0.0;
+	double x = 0.0;
+	double y = 0.0;
+	double t = 0.0;
+
+	public Spike(Image mcImage, double dxdt, double dydt, double x, double y) {
+		this.mcImage = mcImage;
+		this.dxdt = dxdt;
+		this.dydt = dydt;
+		this.x = x;
+		this.y = y;
+	}
+
+	public double jump(double yo, double dydt, double g, double dt, double groundLevel) {
+		double y = yo + dydt*dt + g*dt*dt;
+		if (y > groundLevel) {
+			y = groundLevel;
+		}
+		return y;
+	}
+
+	public void jump(double dydt, double g, double dt, double groundLevel) {
+		t = t + dt;
+		y = y - dydt*t + 0.5*g*t*t;
+		if (y > groundLevel) {
+			y = groundLevel;
+			t = 0;
+			jumping = false;
+		}
+	}
+
+	public void doubleJump(double dydt, double g, double dt, double groundLevel) {
+		t = t + dt;
+		y = y - 0.2*dydt*t + 0.5*g*t*t;
+		if (y > groundLevel) {
+			y = groundLevel;
+			t = 0;
+			doubleJumping = false;
+			jumping = false;
+		}
+	}
+
+	public double slide(double x, double dxdt, double dt) {
+		return x + dxdt*dt;
+	}
+
+	public void slide(double dt) {
+		x = x + dxdt*dt;
+	}
+
+	public boolean getJump() {
+		return jumping;
+	}
+
+	public boolean getDoubleJump() {
+		return doubleJumping;
+	}
+
+	public void setJump(boolean cond) {
+		jumping = cond;
+		t = 0.0;
+	}
+
+	public void setDoubleJump(boolean cond) {
+		doubleJumping = cond;
+		t = 0.0;
+	}
+
+	public boolean isFlying(double groundLevel) {
+		return y < groundLevel;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public double getDxdt() {
+		return dxdt;
+	}
+
+	public double getDydt() {
+		return dydt;
+	}
+
+	public Image getImage() {
+		return mcImage;
+	}
+
+	public void setTime(double time) {
+		double t = time;
+	}
+	public void setX(double newX) {
+		x = newX;
+	}
+
+	public void setY(double newY) {
+		y = newY;
+	}
+
+	public void setImage(Image newImage) {
+		mcImage = newImage;
+	}
+
+	public void setDxdt(double newDxdt) {
+		dxdt = newDxdt;
+	}
+
+	public void setDydt(double newDydt) {
+		dydt = newDydt;
+	}
+}
--- mainWindow.java
+++ mainWindow.java
@@ -16,8 +16,10 @@
 	import javafx.event.ActionEvent;
 	import javafx.event.EventHandler;
 	import javafx.scene.control.*;
+	import javafx.scene.Node;
 	import javafx.stage.*;
 	import javafx.scene.*;
+	import javafx.scene.input.*;
 	import javafx.scene.layout.*;
 	import javafx.scene.control.*;
 	import javafx.geometry.*;
@@ -43,12 +45,17 @@ public class mainWindow extends Application {
 	@SuppressWarnings("unchecked")
 	public void start(Stage stage) {
 
+		int windowHeight = 400;
+		int windowWidth = 800;
+
 		BorderPane gameWindow = new BorderPane();
-		gameWindow.setPrefSize(800, 500);
-		gameWindow.setStyle("-fx-background-image: url(\"/images/background.jpg\");-fx-background-size: 1600, 500;-fx-background-repeat: no-repeat;");
+		gameWindow.setPrefSize(windowWidth, windowHeight);
+		gameWindow.setStyle("-fx-background-color: #000000;");
+
+		Scene scene = new Scene(gameWindow);
 
 		Group root = new Group();
-		Canvas canvas = new Canvas(700,350);
+		Canvas canvas = new Canvas(windowWidth, windowHeight);
 		root.getChildren().add(canvas);
 		GraphicsContext gc = canvas.getGraphicsContext2D();
 
@@ -56,22 +63,56 @@ public class mainWindow extends Application {
 		Image square = new Image("images/square.jpg");
 
 		final long startNanoTime = System.nanoTime();
+		final double dxdt = 0.0; //Speed
+		final double dydt = 20.0; //vertical speed
+		final double g = 75.0; //Restoring gravity
+		final double groundLevel = 332.0; //Ground level position
+		final ArrayList<Double> timeList = new ArrayList<Double>();
+
+		MainCharacter mc = new MainCharacter(square, dxdt, dydt, 100.0, groundLevel);
+
 
 		new AnimationTimer() {
 			@Override
-			public void handle(long currentNanoTime) {
-				final double dxdt = 5.0; //Speed
-				final double dydt = 0.0; //vertical speed
+			public void handle(long currentNanoTime) {		
 				final double t = (currentNanoTime - startNanoTime) / 1000000000.0;
-				final double x = dxdt * t;
-				final double y = dydt * t;
 
-				gc.drawImage(circle, x, y);
+				timeList.add(t);
+				int endLoc = timeList.size();
+				double dt = 0.016;
+				if (endLoc > 2) {
+					dt = timeList.get(endLoc-1) - timeList.get(endLoc-2);
+				}
+
+				final double x = dxdt*t;
+				final double y = groundLevel;
+
+				scene.addEventHandler(KeyEvent.KEY_PRESSED, (key) -> {
+				      if(key.getCode()==KeyCode.SPACE) {
+				          if (!mc.isFlying(groundLevel)) {
+				          	mc.setJump(true);
+				          } else if (mc.getJump() & !mc.getDoubleJump()) {
+				          	mc.setDoubleJump(true);
+				          }
+				      }
+				});
+
+				if (mc.getJump()) {
+					mc.jump(dydt, g, dt, groundLevel);
+				} else if (mc.getDoubleJump()) {
+					mc.doubleJump(dydt, g, dt, groundLevel);
+				} else if (mc.isFlying(groundLevel)) {
+					mc.jump(dydt, g, dt, groundLevel);
+				} else {
+					mc.setTime(0.0);
+				}
+
+				gc.drawImage(gameBackground,0,0);
+				gc.drawImage(mc.getImage(), mc.getX(), mc.getY());
 			}
 		}.start();
 
 		gameWindow.setCenter(root);
-		Scene scene = new Scene(gameWindow);
 		stage.setScene(scene);
 		stage.setResizable(false);
 		stage.setTitle("Run & Jump Man");
