f47889f1ca5dff0e26ab0ad44ed67638f6e39e3b
jmerkl <jacksoncmerkl@gmail.com>
2016-12-30 20:03:44 -0600

    First Rev.
    Got movement on the screen for the gamewindow

--- mainWindow.java
+++ mainWindow.java
@@ -25,6 +25,8 @@
 	import javafx.scene.effect.*;
 	import javafx.scene.paint.*;
 	import javafx.scene.text.*;
+	import javafx.scene.canvas.*;
+	import javafx.animation.*;
 
 	import java.util.*;
 	import javafx.collections.FXCollections;
@@ -41,52 +43,38 @@ public class mainWindow extends Application {
 	@SuppressWarnings("unchecked")
 	public void start(Stage stage) {
 
-		BorderPane window = new BorderPane();
-		window.setPrefSize(600, 500);
-		window.setStyle("-fx-background-color: #007FFF;");
+		BorderPane gameWindow = new BorderPane();
+		gameWindow.setPrefSize(800, 500);
+		gameWindow.setStyle("-fx-background-image: url(\"/images/background.jpg\");-fx-background-size: 1600, 500;-fx-background-repeat: no-repeat;");
 
-		//Top Portion
-			BorderPane titleBox = new BorderPane();
+		Group root = new Group();
+		Canvas canvas = new Canvas(700,350);
+		root.getChildren().add(canvas);
+		GraphicsContext gc = canvas.getGraphicsContext2D();
 
-				Text titleLabel = new Text("Tic Tac Plus");
-				titleLabel.setFont(new Font("Verdana", 40));
-				titleLabel.setFont(Font.font(null, FontWeight.BOLD, 40));
+		Image gameBackground = new Image("images/background.jpg");
+		Image square = new Image("images/square.jpg");
 
-				Button newGameButton = new Button("New Game");
-				newGameButton.setStyle("-fx-font: 14 verdana; -fx-base: #7fd87f;");
-				newGameButton.setPrefSize(100, 40);
+		final long startNanoTime = System.nanoTime();
 
-				Button optionButton = new Button("Options");
-				optionButton.setStyle("-fx-font: 14 verdana; -fx-base: #D3D3D3;");
-				optionButton.setPrefSize(100, 40);
+		new AnimationTimer() {
+			@Override
+			public void handle(long currentNanoTime) {
+				final double dxdt = 5.0; //Speed
+				final double dydt = 0.0; //vertical speed
+				final double t = (currentNanoTime - startNanoTime) / 1000000000.0;
+				final double x = dxdt * t;
+				final double y = dydt * t;
 
-			titleBox.setCenter(titleLabel);
-			titleBox.setRight(optionButton);
-			titleBox.setLeft(newGameButton);
-			titleBox.setStyle("-fx-background-color: #E3E3E3;");
-			titleBox.setPadding(new Insets(10,10,10,10));
-		
-		window.setTop(titleBox);
+				gc.drawImage(circle, x, y);
+			}
+		}.start();
 
-		//Bottom Layout
-			HBox bottomBoxPrev = new HBox(10);
-
-				turnLabel.setFont(new Font("Verdana", 30));
-				turnLabel.setFont(Font.font(null, FontWeight.BOLD, 30));
-
-			bottomBoxPrev.getChildren().addAll(turnLabel);
-			bottomBoxPrev.setPadding(new Insets(10,10,10,10));
-			bottomBoxPrev.setAlignment(Pos.CENTER);
-			bottomBoxPrev.setStyle("-fx-background-color: #ffd27f");
-
-		window.setBottom(bottomBoxPrev);
-
-		//Set Scene
-
-			Scene scene = new Scene(window);
-			stage.setScene(scene);
-			stage.setResizable(false);
-			stage.setTitle("Shitty Tic Tac Toe Game");
-			stage.show();
+		gameWindow.setCenter(root);
+		Scene scene = new Scene(gameWindow);
+		stage.setScene(scene);
+		stage.setResizable(false);
+		stage.setTitle("Run & Jump Man");
+		stage.show();
 	}
 }
